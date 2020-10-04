f1f49e516fe12cc9378b5c635757d424816c3af5
Andrey Sivak <riziy_bes@mail.ru>
2020-10-05 00:43:44 +0300

    completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
+import { Menu } from "./Menu";
+
 window.addEventListener('load', function () {
 
-    console.log(1);
+    const menu = new Menu({
+        button: document.querySelector('.header-menu__btn'),
+        hamburger: document.querySelector('.header-menu__burger'),
+        menu: document.querySelector('.header-menu'),
+        activeClass: 'active'
+    });
+
+    menu.init();
 
 });
